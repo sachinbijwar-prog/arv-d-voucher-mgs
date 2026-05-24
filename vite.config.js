4a2a3224808c90ef0776acf879d8b722f2bb2439
sachin bijwar <sachin.bijwar@smart-analytica.com>
2026-05-24 22:13:09 +0530

    Revert Vite base to fix GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ const REPO_NAME = 'arv-d-voucher-mgs' // ← your GitHub repo name
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/arv-d-voucher-mgs/' : '/',
   resolve: {
     alias: {
       '@': '/src',
